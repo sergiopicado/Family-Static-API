28b20a97dfdb6b68100321c1fd23ea88cc04c55c
sergiopicado <sergiopicado@gmail.com>
2021-03-22 17:08:56 +0000

    Actualizacion

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, json, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
@@ -31,12 +31,27 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/members', methods=['POST'])
+def add_memberApp():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    print(decoded_object)
+    jackson_family.add_memberDataStr(decoded_object)
+    print("Incoming request with the following body", decoded_object)
+    return jsonify(jackson_family.get_all_members()),200
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_memberApp(id):
+    for i in range (len(jackson_family.get_all_members())):
+        if jackson_family.get_all_members()[i]["id"] == id:
+            jackson_family.get_all_members().pop(i)
+            break
+    print("This is the position to delete: ",id)
+    return jsonify(jackson_family.get_all_members()),200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,28 +8,36 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {"id": self._generateId(), "Name": "John Jackson", "Age": 33,
+             "Lucky Numbers": [7, 13, 22]},
+            {"id": self._generateId(), "Name": "Jane Jackson", "Age": 33,
+             "Lucky Numbers": [10, 14, 3]},
+            {"id": self._generateId(), "Name": "Jimmy Jackson",
+             "Age": 5, "Lucky Numbers": [1]}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
-        # fill this method and update the return
-        pass
+    def add_memberDataStr(self, member):
+        member["id"] = self._generateId()
+        print(member)
+        self._members.append(member)
 
-    def delete_member(self, id):
-        # fill this method and update the return
-        pass
+    def delete_memberDataStr(self, id):
+        self._members.pop(member)
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        json_text = jsonify(self._members)
+        return json_text
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
